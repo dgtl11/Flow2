25de60b3fb21bf2bd5c00b3a3b0df4beb3244f2f
dgtl11 <digital11@gmx.de>
2019-03-27 09:17:31 +0100

    work-place

---
+++ src/chpt01/TestClass.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// <editor-fold defaultstate="collapsed" desc="Title">
+/*     Buch: - Flow2 / OCP Study Guide
+ *  Kapitel: - 01 -
+ *
+ *    Thema: - 1.0 - Introduction
+ *
+ *  Listing: - 1.0.00 - TestClass.java
+ *  
+ *   IDEA Project
+ *
+ *    Autor: - Daniel dgtl
+ *  </editor-fold> */
+
+// import:
+
+package chpt01;
+
+public class TestClass {
+    public static void main(String[] args) {
+        
+        
+    }
+}
