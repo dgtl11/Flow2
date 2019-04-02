3fec16981f255104eff7943630e825835c8e3cad
dgtl11 <digital11@gmx.de>
2019-04-02 16:26:37 +0200

    work-place

--- src/chpt02/TestClass02.java
+++ src/chpt02/TestClass02.java
@@ -15,9 +15,19 @@
 
 package chpt02;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class TestClass02 {
+
     public static void main(String[] args) {
-        
+
+        List<StringBuilder> messages = Arrays.asList(new StringBuilder(), new StringBuilder());
+        messages.stream().forEach(s->s.append("helloworld"));
+        messages.forEach(s->{
+            s.insert(5,",");
+            System.out.println(s);
+        });
         
     }
 }
