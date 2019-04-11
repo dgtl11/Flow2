06f0cc8ec40d0c626fcb283f0f658e71ebd6dc0d
dgtl11 <digital11@gmx.de>
2019-04-11 16:25:18 +0200

    work-place

--- src/chpt04/streams/ReductionTest.java
+++ src/chpt04/streams/ReductionTest.java
@@ -13,7 +13,8 @@
 
 package chpt04.streams;
 
-import java.util.TreeSet;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ReductionTest {
@@ -21,15 +22,18 @@ public class ReductionTest {
     public static void main(String[] args) {
         
         Stream<String> str = Stream.of("H","a","l","l","o");
+
+        // immutable Reduction
         // System.out.println(str.reduce("", (a, b) -> a + b));
 
+        // Mutable Reduction
         // StringBuilder erg = str.collect(StringBuilder::new, StringBuilder::append, StringBuilder::append);
-        // str.collect(StringBuilder::new, StringBuilder::append, StringBuilder::append);
+        // TreeSet<String> erg = str.collect(TreeSet::new, TreeSet::add, TreeSet::addAll);
+        // ArrayList<String> erg = str.collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
 
-        // StringBuilder sb = new StringBuilder();
-        // sb.append()
-
-        TreeSet<String> erg = str.collect(TreeSet::new, TreeSet::add, TreeSet::addAll);
+        // List<String> erg = str.collect(Collectors.toList());
+        // Set<String> erg = str.collect(Collectors.toSet());
+        Collection<String> erg = str.collect(Collectors.toCollection(HashSet::new));
 
         System.out.println(erg);
     }
