a60aeb0518b6d6dad8a732c3b8fc88c5fcd1767a
dgtl11 <digital11@gmx.de>
2019-05-19 13:47:18 +0200

    home-office

--- src/functionalProgramming/FPinJava8.java
+++ src/functionalProgramming/FPinJava8.java
@@ -1,5 +1,5 @@
 // <editor-fold defaultstate="collapsed" desc="Title">
-/*     Buch: - YouTube Tutorials
+/*     Buch: - YouTube Lessons & Xmpls
  *  Kapitel: - 12 - Functional Programming in Java 8
  *
  *    Thema: - 12.0 - Lambdas & Streams
@@ -14,10 +14,42 @@
 package functionalProgramming;
 
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
 public class FPinJava8 {
     public static void main(String[] args) {
 
-        
+        List<Integer> values = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        // external Iterator
+/*        for(int i = 0; i < values.size(); i++) {
+            System.out.println(values.get(i));
+        }
+*/
+
+/*        for(int e : values) {
+            System.out.println(e);
+        }
+*/
+
+        // internal iterator
+/*        values.forEach(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer value) {
+                System.out.println(value);
+            }
+        });
+*/
+
+//        values.forEach((Integer value) -> System.out.println(value));
+
+//        values.forEach(value-> System.out.println(value));
+
+//        values.forEach(value -> System.out.println(value));
+
+        values.forEach(System.out::println);
 
     }
 }
