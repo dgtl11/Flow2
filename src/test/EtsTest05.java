2e6a658a3803663e049e6be568a1b41193666dcc
dgtl11 <digital11@gmx.de>
2019-05-27 13:59:24 +0200

    work-place

--- src/test/EtsTest05.java
+++ src/test/EtsTest05.java
@@ -13,10 +13,30 @@
 
 package test;
 
+class A {
+    char  c;
+    A(char c) { this.c = c;}
+}
+
+class B extends A {
+    char c = 'a';
+    B() { super('b');}
+    class C extends A {
+        char c = 'c';
+        C() {
+            super('d');
+            System.out.println(B.this.c);
+            System.out.println(C.this.c);
+            System.out.println(super.c);
+
+        }
+    }
+}
+
 public class EtsTest05 {
     public static void main(String[] args) {
 
-
+        B.C obj = new B().new C();
 
     }
 }
