6de9e359afac642bed52034611e89225310cf87d
dgtl11 <digital11@gmx.de>
2019-05-27 08:51:02 +0200

    work-place

---
+++ src/test/EtsTest05.java
@@ -0,0 +1,22 @@
+// <editor-fold defaultstate="collapsed" desc="Title">
+/*     Buch: - Flow2 / OCP Study Guide
+ *  Kapitel: - 11 - AllTopicsAtOnce
+ *
+ *    Thema: - 11.0 - Ets Test 05
+ *
+ *  Listing: - 11.0.05 - EtsTest05.java
+ *  
+ *       IDEA Project
+ *
+ *    Autor: - Daniel dgtl
+ *  </editor-fold> */
+
+package test;
+
+public class EtsTest05 {
+    public static void main(String[] args) {
+
+
+
+    }
+}
